987553996460d7ceacbf499b1aa3d22c7cdd59bc
Ivan Klymchuk <iklymchuk@anchorfree.com>
2018-10-08 14:17:36 -0700

    Add backup script and some more bkups

---
+++ bkupesp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PORT=/dev/tty.wchusbserial141140
+
+esptool.py --port ${PORT} --baud 921600 chip_id > bkupesp.tmp
+CHIP_ID=$(cat bkupesp.tmp | grep "Chip ID: 0x" | cut -d "x" -f2-)
+
+BKUP_NAME=backup_orig_${CHIP_ID}.bin
+
+esptool.py --port ${PORT} --baud 921600 read_flash 0x0 0x400000 ${BKUP_NAME}
+echo Backup in ${BKUP_NAME}
