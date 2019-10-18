c11c76220184986ac8f50a23a269ca0f0cdec07e
lim <lim@lims-MBP.attlocal.net>
2019-10-17 21:44:36 -0700

    ttgo tft default

--- bkupesp.sh
+++ bkupesp.sh
@@ -2,12 +2,13 @@
 
 #PORT=/dev/tty.wchusbserial141140
 #PORT=/dev/tty.wchusbserial14140
-PORT=/dev/tty.SLAB_USBtoUART
+PORT=/dev/tty.SLAB_USBtoUART9
 
 esptool.py --port ${PORT} --baud 921600 chip_id > bkupesp.tmp
 CHIP_ID=$(cat bkupesp.tmp | grep "Chip ID: 0x" | cut -d "x" -f2-)
 
 BKUP_NAME=backup_orig_${CHIP_ID}.bin
 
-esptool.py --port ${PORT} --baud 921600 read_flash 0x0 0x400000 ${BKUP_NAME}
+#esptool.py --port ${PORT} --baud 921600 read_flash 0x0 0x400000 ${BKUP_NAME}
+esptool.py --port ${PORT} --baud 115200 read_flash 0x0 0x400000 ${BKUP_NAME}
 echo Backup in ${BKUP_NAME}
